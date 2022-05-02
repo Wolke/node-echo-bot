11b79b7997d27868fad0c9d7c2c57b0db7edbeaa
Wolke <wolkesau@gmail.com>
2022-05-02 08:24:22 +0000

    ok

--- index.js
+++ index.js
@@ -16,22 +16,42 @@ app.get(`/`, async (req, rep) => {
   let { locationName, weatherElement } = json.records.location[0];
   let parameter = weatherElement[1].time[0].parameter;
   console.log(parameter);
-  rep.end(`${parameter.parameterName} %`);
+  let reponse = `${parameter.parameterName} %`
+  rep.end(reponse);
 });
 app.post(`/webhook`, line.middleware(CONFIG), (req, rep) => {
   console.log(req.body.events);
-  let event = req.body.events[0];
-  let { text } = event.message;
-  if (text.search(`天氣`) > 0) {
-    // search
-  } else {
-    text = `我不明白`;
-  }
+  req.body.events.map(async e=>{
+    let text= e.message.text;
+    if(text.search(`天氣`)>-1){
+      let url = `https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-C0032-001?Authorization=CWB-79D4FC47-04F4-436C-BB72-993ECDF752C8&limit=1&locationName=`;
 
-  client.replyMessage(event.replyToken, {
-    type: `text`,
-    text: text,
-  });
+      let json = await fetch(url).then((r) => r.json());
+      //console.log(json);
+      let { locationName, weatherElement } = json.records.location[0];
+      let parameter = weatherElement[1].time[0].parameter;
+      console.log(parameter);
+      text = `${locationName} 的降雨機率是 ${parameter.parameterName} %`
+    }
+    
+    client.replyMessage(e.replyToken, {
+      type: `text`,
+      text: text,
+    });
+  })
+
+  // let event = req.body.events[0];
+  // let { text } = event.message;
+  // if (text.search(`天氣`) > 0) {
+  //   // search
+  // } else {
+  //   text = `我不明白`;
+  // }
+
+  // client.replyMessage(event.replyToken, {
+  //   type: `text`,
+  //   text: text,
+  // });
 });
 app.listen(8080, () => {
   console.log(`running`);
